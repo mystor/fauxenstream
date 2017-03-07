a99304e815b38d4c1a49aa5d1a10f1a5f2825aca
Michael Layzell <michael@thelayzells.com>
2017-03-06 23:09:10 -0500

    Update documentation

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["fauxenstream"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='fauxenstream/struct.LexError.html' title='fauxenstream::LexError'>LexError</a>",];
+implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
